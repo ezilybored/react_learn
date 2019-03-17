e427bea1f7112ec4afa400288847c9ede3e1fc5b
Ben Rogers <33623970+ezilybored@users.noreply.github.com>
2019-03-17 21:17:01 +0000

    vidly: basic table with delete button. One component movies.jsx

---
+++ vidly/src/components/movies.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    // Gets the movie list from fakeMovieServices
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    console.log(movie);
+    const movies = this.state.movies.filter(c => c._id !== movie._id);
+    this.setState({ movies: movies });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Movies Component</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm m-2"
+                    onClick={() => this.handleDelete(movie)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
