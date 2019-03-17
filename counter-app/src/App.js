aaddf9b4e74f3c8ef71cca4cad4cbfa3fcec6dfb
Ben Rogers <33623970+ezilybored@users.noreply.github.com>
2019-03-17 10:39:28 +0000

    example of mounting phase use

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,127 +1,128 @@
-import React, { Component } from 'react';
-import './App.css';
-import Counters from "./components/counters"
-import Navbar from "./components/navbar"
+import React, { Component } from "react";
+import "./App.css";
+import Counters from "./components/counters";
+import Navbar from "./components/navbar";
 
 class App extends Component {
+  // All pasted code that previously resided in counters.jsx. Moved so that other components can access the state
+  state = {
+    // An array of counters for each object that stores some infor on each product
+    counters: [
+      { id: 1, value: 0, cost: 2, product: "Bread" },
+      { id: 2, value: 0, cost: 1, product: "Jam" },
+      { id: 3, value: 0, cost: 3, product: "Spoons" },
+      { id: 4, value: 0, cost: 9000, product: "Freight train" }
+    ],
 
-    // All pasted code that previously resided in counters.jsx. Moved so that other components can access the state
-    state = {
-      // An array of counters for each object that stores some infor on each product
-      counters: [
-        { id: 1, value: 0, cost: 2, product: 'Bread' },
-        { id: 2, value: 0, cost: 1, product: 'Jam' },
-        { id: 3, value: 0, cost: 3, product: 'Spoons'},
-        { id: 4, value: 0, cost: 9000, product: 'Freight train' },
-      ],
+    total: 0
+  };
 
-      total: 0
+  // Lifecycle Hooks (Mounting)
+  // Can only be used in class components not in stateless functional components
+  // Constructor is called once when a component is initialised.
+  // In app it is a good place to initialise the state using props passed in as arguments
+  constructor(props) {
+    super();
+    console.log("App - constructor", props);
+    // this.state = this.props.something...
+    // Make sure to use this.state not setState()
+  }
+  // componentDidMount() Called after the component is rendered into the DOM
+  componentDidMount() {
+    // Good for making AJAX calls
+    // E.g. this.setState({ New data from server})
+    // Example of use is in counter.jsx
+  }
+  // The render method is the third lifecycle hook related to mounting.
+  // This has been used extensively all ready
+  // render mehod is called before componentDidMount.
+
+  // counterID sets the product to delete from the list
+  handleDelete = counterId => {
+    console.log("Event Handler Called on ", counterId);
+    // Creates a new array filtering out any entries where the id is the same as the id passed from the child
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    // Sets the value of counters in the state object with the value in the counters constant just set
+    this.setState({ counters: counters });
+
+    const total = 0;
+    this.setState({ total: total });
+  };
+
+  // Resets all values to 0 when the reset button is pressed
+  handleReset = counter => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters: counters });
+
+    const countersreset = [...this.state.counters];
+    // Find the index of the counter passed in by the child on the event click
+    const index = countersreset.indexOf(counter);
+    countersreset[index] = { ...counter };
+    const total = 0;
+    this.setState({ total: total });
+  };
+
+  // Event handlers
+  // Add in an argument called product. Thsi could be from an object containing a list of products
+  handleIncrement = counter => {
+    // The state of the component is changed indirectly using setState
+    // Use spread operator to clone the counters array
+    const counters = [...this.state.counters];
+    // Find the index of the counter passed in by the child on the event click
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value = counters[index].value + counters[index].cost;
+    this.setState({ counters: counters });
+
+    const total = this.state.total + counters[index].cost;
+    this.setState({ total: total });
+  };
+
+  // An event to handle the emptying of a purchase amount
+  handleClear = counter => {
+    const counters = [...this.state.counters];
+    // Find the index of the counter passed in by the child on the event click
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    const total = this.state.total - counters[index].value;
+    this.setState({ total: total });
+    counters[index].value = 0;
+    this.setState({ counters: counters });
+  };
+
+  // An event to handle the removal of a single item
+  handleRemoveOne = counter => {
+    const counters = [...this.state.counters];
+    // Find the index of the counter passed in by the child on the event click
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    if (counters[index].value > 0) {
+      counters[index].value = counters[index].value - counters[index].cost;
+      this.setState({ counters: counters });
+
+      const total = this.state.total - counters[index].cost;
+      this.setState({ total: total });
     }
-
-    // Lifecycle Hooks (Mounting)
-    // Constructor is called once when a component is initialised.
-    // In app it is a good place to initialise the state using props passed in as arguments
-    constructor(props) {
-      super()
-      console.log('App - constructor', props)
-      // this.state = this.props.something...
-      // Make sure to use this.state not setState()
-    }
-    // componentDidMount() Called after the component is rendered into the DOM
-    componentDidMount() {
-      // Good for making AJAX calls
-      // E.g. this.setState({ New data from server})
-    }
-    // The render method is the third lifecycle hook related to mounting.
-    // This has been used extensively all ready
-
-    // counterID sets the product to delete from the list
-    handleDelete = (counterId) => {
-      console.log('Event Handler Called on ', counterId)
-      // Creates a new array filtering out any entries where the id is the same as the id passed from the child
-      const counters = this.state.counters.filter(c => c.id !== counterId)
-      // Sets the value of counters in the state object with the value in the counters constant just set
-      this.setState({ counters: counters })
-
-      const total = 0
-      this.setState({ total: total })
-    }
-
-    // Resets all values to 0 when the reset button is pressed
-    handleReset = counter => {
-
-      const counters = this.state.counters.map(c => {
-        c.value = 0
-        return c
-      })
-      this.setState({ counters: counters })
-
-      const countersreset = [...this.state.counters]
-      // Find the index of the counter passed in by the child on the event click
-      const index = countersreset.indexOf(counter)
-      countersreset[index] = {...counter}
-      const total = 0
-      this.setState({ total: total })
-    }
-
-    // Event handlers
-    // Add in an argument called product. Thsi could be from an object containing a list of products
-    handleIncrement = counter => {
-      // The state of the component is changed indirectly using setState
-      // Use spread operator to clone the counters array
-      const counters = [...this.state.counters]
-      // Find the index of the counter passed in by the child on the event click
-      const index = counters.indexOf(counter)
-      counters[index] = {...counter}
-      counters[index].value = counters[index].value + counters[index].cost
-      this.setState({ counters: counters })
-
-      const total = this.state.total + counters[index].cost
-      this.setState({ total: total })
-    }
-
-    // An event to handle the emptying of a purchase amount
-    handleClear = counter => {
-      const counters = [...this.state.counters]
-      // Find the index of the counter passed in by the child on the event click
-      const index = counters.indexOf(counter)
-      counters[index] = {...counter}
-      const total = this.state.total - counters[index].value
-      this.setState({ total: total })
-      counters[index].value = 0
-      this.setState({ counters: counters })
-
-    }
-
-    // An event to handle the removal of a single item
-    handleRemoveOne= counter => {
-      const counters = [...this.state.counters]
-      // Find the index of the counter passed in by the child on the event click
-      const index = counters.indexOf(counter)
-      counters[index] = {...counter}
-      if (counters[index].value > 0){
-        counters[index].value = counters[index].value - counters[index].cost
-        this.setState({ counters: counters })
-
-        const total = this.state.total - counters[index].cost
-        this.setState({ total: total })
-      }
-    }
+  };
 
   render() {
     return (
       <React.Fragment>
-      {/* Passes the total number of items to the Navbar. Filters out any with values of 0 */}
-        <Navbar totalCount={this.state.total}/>
-          <main className="container">
-            <Counters onDelete={this.handleDelete}
-                      onIncrement={this.handleIncrement}
-                      onClear={this.handleClear}
-                      onRemoveOne={this.handleRemoveOne}
-                      onReset={this.handleReset}
-                      counters={this.state.counters}
-            />
-          </main>
+        {/* Passes the total number of items to the Navbar. Filters out any with values of 0 */}
+        <Navbar totalCount={this.state.total} />
+        <main className="container">
+          <Counters
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onClear={this.handleClear}
+            onRemoveOne={this.handleRemoveOne}
+            onReset={this.handleReset}
+            counters={this.state.counters}
+          />
+        </main>
       </React.Fragment>
     );
   }
--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,67 +1,80 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 //Extend Component to inherit methiods from Component
 class Counter extends Component {
-  state = {
-
+  // looks at the previous state of the component
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+    // Can use this to compare to the current state and make an AJAX server call if there is a change
+    if (prevProps.counter.value !== this props.counter.value) {
+      // Make AJAX call
+    }
   }
+
+  state = {};
   // 3 ways of CSS styling. Inline, as an object, or as a separate file
   styles = {
     fontSize: 20,
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold"
+  };
 
   render() {
-
     return (
       <React.Fragment>
-        <span
-          style={ this.styles }
-          className={this.getBadgeClasses()}>{this.formatCount()}
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
         </span>
         <button
-          style={{ fontSize: 20, fontWeight: 'bold' }}
-          onClick={ () => this.props.onIncrement(this.props.counter) }
-          className="btn btn-primary btn-sm m-2">{ this.props.counter.product }
+          style={{ fontSize: 20, fontWeight: "bold" }}
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-primary btn-sm m-2"
+        >
+          {this.props.counter.product}
         </button>
-        <span
-          style={ this.styles }
-          className="btn btn-info btn-sm m-2">$ { this.props.counter.cost }
+        <span style={this.styles} className="btn btn-info btn-sm m-2">
+          $ {this.props.counter.cost}
         </span>
         <button
-          style={{ fontSize: 20, fontWeight: 'bold' }}
-          onClick={ () => this.props.onRemoveOne(this.props.counter) }
-          className="btn btn-primary btn-sm m-2">Remove Item
+          style={{ fontSize: 20, fontWeight: "bold" }}
+          onClick={() => this.props.onRemoveOne(this.props.counter)}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Remove Item
         </button>
         <button
-          style={{ fontSize: 20, fontWeight: 'bold' }}
-          onClick={ () => this.props.onClear(this.props.counter) }
-          className="btn btn-warning btn-sm m-2">Clear
+          style={{ fontSize: 20, fontWeight: "bold" }}
+          onClick={() => this.props.onClear(this.props.counter)}
+          className="btn btn-warning btn-sm m-2"
+        >
+          Clear
         </button>
         {/* onClick={ () => this.props.onDelete(this.props.id) } this passes the ID of the product to remove back to the parent when the call is made */}
         <button
-          style={{ fontSize: 20, fontWeight: 'bold' }}
-          onClick={ () => this.props.onDelete(this.props.counter.id) }
-          className="btn btn-danger btn-sm m-2">Delete
+          style={{ fontSize: 20, fontWeight: "bold" }}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </React.Fragment>
-    )
+    );
   }
 
   // Setting the colour of the cart number
   getBadgeClasses() {
-    let classes = "badge m-2 badge-"
+    let classes = "badge m-2 badge-";
     // Appends either warning or primary to the end of classes depending on its value === 0 or not
-    classes += (this.props.counter.value === 0) ? "secondary" : "default"
-    return classes
+    classes += this.props.counter.value === 0 ? "secondary" : "default";
+    return classes;
   }
 
   formatCount() {
     // Stores the count value from this.state as a new const count
-    const { value } = this.props.counter
+    const { value } = this.props.counter;
     // If this.state.count is 0 show Zero. If not then show the value of this.state.count
-    return value === 0 ? 'Zero' : value
+    return value === 0 ? "Zero" : value;
   }
 }
 
-export default Counter
+export default Counter;
