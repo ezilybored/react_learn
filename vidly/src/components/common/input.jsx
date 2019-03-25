06d902cb4aa470ea480a83a04d89a628e5a265e3
Ben Rogers <33623970+ezilybored@users.noreply.github.com>
2019-03-25 10:31:18 +0000

    extracted reusable version of the form component

--- vidly/src/components/common/input.jsx
+++ vidly/src/components/common/input.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 
 // Destructure props as arguments
-const Input = ({ name, label, value, error, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        id={name}
-        name={name}
-        type="text"
-        className="form-control"
-      />
+      {/* Allow for more inpout fields using the rest/spread operator. id, name and error are the only essential(constant) properties */}
+      <input {...rest} name={name} id={name} className="form-control" />
       {/* If error is truthy then the div is rendered, if not then it won't be */}
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
