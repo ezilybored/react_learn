64c5361edc144cbb045642cc04d2020ececabec2
Ben Rogers <33623970+ezilybored@users.noreply.github.com>
2019-03-25 09:23:59 +0000

    improved validation with Joi

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -6,30 +7,27 @@ class LoginForm extends Component {
     account: { username: "", password: "" },
     errors: {}
   };
-  // Use the hook componentDidMount and ref to set focus when the page loads
-  // This could be done by setting the autoFocus attribute on the <input> tag
-  /*
-  username = React.createRef();
 
-  componentDidMount() {
-    this.username.current.focus();
-  }
-  */
+  // The Joi schema for validation
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
 
   // The basic form validation function
   validate = () => {
+    // Options for Joi
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
     const errors = {};
-
-    const { account } = this.state;
-
-    if (account.username.trim() === "")
-      errors.username = "Username is required.";
-
-    if (account.password.trim() === "")
-      errors.password = "Password is required.";
-
-    // If the number of keys in the error object is 0 then return null, if not then return the errors object
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = e => {
@@ -48,14 +46,10 @@ class LoginForm extends Component {
   };
 
   validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required.";
-      // Other rules could be implemented
-    }
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required.";
-      // Other rules could be implemented
-    }
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleChange = ({ currentTarget: input }) => {
