280bf9f17e5d976da391e2a844569b4ca7716fd1
Ben Rogers <33623970+ezilybored@users.noreply.github.com>
2019-03-27 13:40:21 +0000

    refactored the frontend CRUD features

--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -32,15 +32,16 @@ class MovieForm extends Form {
       .label("Rate")
   };
 
-  async componentDidMount() {
+  async populateGenres() {
     const { data: genres } = await getGenres();
     this.setState({ genres: genres });
+  }
 
-    // Reads and stores the movie id parameter from the route
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
-
+  async populateMovies() {
     try {
+      // Reads and stores the movie id parameter from the route
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
       const { data: movie } = await getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
     } catch (ex) {
@@ -48,6 +49,11 @@ class MovieForm extends Form {
       if (ex.response && ex.response.status === 404)
         return this.props.history.replace("/not-found");
     }
+  }
+
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovies();
   }
 
   // This function maps the data from the server to fit the model required for the current page
--- vidly/src/services/movieService.js
+++ vidly/src/services/movieService.js
@@ -1,25 +1,29 @@
-//import * as genresAPI from "./fakeGenreService";
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 
+const apiEndpoint = apiUrl + "/movies";
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
-  return http.get(apiUrl + "/movies");
+  return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiUrl + "/movies" + "/" + movieId);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    http.put(apiUrl + "/movies" + "/" + movie._id, body);
+    http.put(movieUrl(movie._id), body);
   } else {
-    http.post(apiUrl + "/movies", movie);
+    http.post(apiEndpoint, movie);
   }
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiUrl + "/movies" + "/" + movieId);
+  return http.delete(movieUrl(movieId));
 }
